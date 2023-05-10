b602a802bbf724edda50eb5119e2736cab26331c
cmruizg777 <44457004+cmruizg777@users.noreply.github.com>
2023-05-10 12:30:57 -0500

    Update enviar_mail.php

--- acciones/enviar_mail.php
+++ acciones/enviar_mail.php
@@ -53,7 +53,7 @@ try {
             die("Error al descargar el archivo PDF");
         }
         */
-        //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
